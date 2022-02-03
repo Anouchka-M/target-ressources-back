5bed08230193e8733f3e7e577c7345e371dd68dc
Anouchka <anouchka.maurer@protonmail.com>
2022-02-03 22:41:08 +0100

    petites corrections

--- src/routes/ressources.js
+++ src/routes/ressources.js
@@ -7,47 +7,39 @@ Router.get('/', (req, res) => {
 })
 
 Router.get('/read', (req, res) => {
-    let sql = 'SELECT * FROM ressources'
-  
-    connection.query(sql, (err, result) => {
-      if (err) {
-        console.error(err)
-        res.status(500).send('Error retrieving ressources from database')
-      } else {
-        res.json(result)
-      }
-    })
-  })
-  
+  let sql = 'SELECT * FROM ressources'
 
-  Router.post("/add", (req, res) => {
-    const sql = "INSERT INTO ressources (`type`, `picto`, `x`, `y`, `indications`) VALUES (?,?,?,?,?)";
-    const values = [
-      req.body.type,
-      req.body.picto,
-      req.body.x,
-      req.body.y,
-      req.body.indications,
-    ]
-    console.log("POST '/ressources/add'")
-    
-    connection.query(sql, values, (err, result) => {
-      if (err) throw err;
-      return res.status(200).send(result);
-    })
+  connection.query(sql, (err, result) => {
+    if (err) {
+      console.error(err)
+      res.status(500).send('Error retrieving ressources from database')
+    } else {
+      res.json(result)
+    }
   })
-  
-    Router.delete("/delete", (req, res) => {
-    const sql = "DELETE FROM ressources WHERE id=?";
-    const values = [
-      req.body.id
-    ]
-    console.log("DELETE '/ressources/delete'")
-    
-    connection.query(sql, values, (err, result) => {
-      if (err) throw err;
-      return res.status(200).send(result);
-    })
-    })
-  
-module.export = Router
+})
+
+Router.post('/add', (req, res) => {
+  const sql =
+    'INSERT INTO ressources (`type`, `x`, `y`, `indications`) VALUES (?,?,?,?)'
+  const values = [req.body.type, req.body.x, req.body.y, req.body.indications]
+  console.log("POST '/ressources/add'")
+
+  connection.query(sql, values, (err, result) => {
+    if (err) throw err
+    return res.status(200).send(result)
+  })
+})
+
+Router.delete('/delete', (req, res) => {
+  const sql = 'DELETE FROM ressources WHERE id=?'
+  const values = [req.body.id]
+  console.log("DELETE '/ressources/delete'")
+
+  connection.query(sql, values, (err, result) => {
+    if (err) throw err
+    return res.status(200).send(result)
+  })
+})
+
+module.exports = Router
